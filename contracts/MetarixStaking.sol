1d1075fa929f1a8326c9ab5005987640e5854c57
Socarde Paul <84129217+DRIVENpol@users.noreply.github.com>
2022-12-28 21:14:26 +0200

    Update MetarixStaking.sol

--- contracts/MetarixStaking.sol
+++ contracts/MetarixStaking.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IToken {
     function balanceOf(address account) external view returns (uint256);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
 /**
